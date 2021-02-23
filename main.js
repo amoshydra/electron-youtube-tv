b75e228ed526d818f9a52c3d63ff94c411430b0b
Guilherme Fernandes <57098649+sigma-guilhfer@users.noreply.github.com>
2021-02-23 00:08:25 -0300

    working update (electron + user agent)
    Updated the code for the new version of electron and changed the User-Agent for it stop redirecting from https://youtube.com/tv

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 'use strict';
 
-const electron = require('electron');
+const {app, BrowserWindow, webContents, session } = require('electron');
 const urlChecker = require('./libs/urlChecker');
 const tray = require('./controllers/tray');
-const { app, BrowserWindow, webContents } = electron;
 
 app.on('ready', function() {
   var mainWindow = new BrowserWindow({
     title: 'Youtube TV',
-    width: 800,
-    height: 600,
+    width: 1240,
+    height: 720,
     icon: __dirname + '/favicon.ico',
     show: false
   });
   mainWindow.setMenu(null);
   tray.init(mainWindow);
+
+  session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+    details.requestHeaders['User-Agent'] = 'Mozilla/5.0 (SMART-TV; Linux; Tizen 2.4.0) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.1 TV Safari/538.1';
+    callback({ cancel: false, requestHeaders: details.requestHeaders });
+  });
 
   mainWindow.webContents.on('did-navigate-in-page', (event, url, isMainFrame) => {
     urlChecker.init(url);
